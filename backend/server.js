b7e9213901d26d85ca9782c5123bc1f9f23c28ed
Arosh59 <aroshdanza59@gmail.com>
2026-01-08 16:22:00 +0530

    Project store

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
+import mongoose from "mongoose";
+
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json()); // allows us to accept JSON data in request body
+
+app.use("/api/products", productRoutes);
+
+app.listen(5000, () => {
+    connectDB();
+    console.log("Server started at http://localhost:5000");
+});
